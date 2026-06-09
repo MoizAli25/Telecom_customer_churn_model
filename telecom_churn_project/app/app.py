860dcd8a96e25adbd2b315f79b687e9a52d58c85
MoizAli25 <161498237+MoizAli25@users.noreply.github.com>
2026-06-10 01:25:06 +0500

    Fix CSS file path in load_css function

--- telecom_churn_project/app/app.py
+++ telecom_churn_project/app/app.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 # ── Load CSS ──
 def load_css():
-    with open("app/styles.css") as f:
+    with open("styles.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 load_css()
